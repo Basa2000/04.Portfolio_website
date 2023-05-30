34b19255c22f7af45448778f98ff6436b6be33eb
Kumbhar-github <basav.futurekraft@gmail.com>
2023-05-30 20:53:12 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        "sm":"480px"
+      }
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+
+    }
+  },
+  plugins: [],
+}
+
